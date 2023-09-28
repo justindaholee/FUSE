f845b1cc928ec8a2b0da2daedecf237234ea1f0e
shanizu <77214955+shanizu@users.noreply.github.com>
2023-09-28 15:00:23 -0700

    updates release version to show beta status

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
   
 setup(
     name='fuse_toolkit',
-    version='0.1.0',
+    version='0.1.0b1',
     description='FUSE toolkit supports fluorescent cell image alignment and analysis.',
     author='Shani Zuniga',
     author_email='shani.zuniga@gmail.com',

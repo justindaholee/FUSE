0c425da0e919e920e0a62bde6d70265df16fd504
shanizu <77214955+shanizu@users.noreply.github.com>
2023-09-29 14:20:37 -0700

    add metadata and long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
   
 setup(
     name='fuse_toolkit',
-    version='0.1.0b2',
+    version='0.1.0b3',
     description='FUSE toolkit supports fluorescent cell image alignment and analysis.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Shani Zuniga',
     author_email='shani.zuniga@gmail.com',
     license='MIT',

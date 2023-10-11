84f1c19f204099746f752c7345d6811b3e25d340
shanizu <77214955+shanizu@users.noreply.github.com>
2023-10-11 15:46:37 -0700

    modifies exp tests

--- tests/experiment_test.py
+++ tests/experiment_test.py
@@ -1,31 +1,32 @@
 import os
 import shutil
 import pytest
-from fuse_toolkit import Experiment # Import the Experiment class from your module
+from fuse_toolkit import Experiment
 
 class TestExperimentClass:
     
     @pytest.fixture
     def setup(self):
-        print("setting up...")
-        self.test_folder = os.path.join('tmp', 'test_experiment')
-        os.makedirs(self.test_folder)
-        self.date = "2023-10-11"
-        self.expID = "TestExp"
-        self.path = os.path.join(self.test_folder, "test_data")
-        os.makedirs(self.path)
-        self.parseID = "Name_Well"
-        self.separator = "_"
-        self.channelInfo = "Fluor"
-        self.channelToSegment = "Fluor"
+        print('Setting up...')
+        self.test_folder = os.path.join('tests', 'test_resources', "")
+        self.date = '2023-10-11'
+        self.expID = 'TestExp'
+        self.path = os.path.join('tests', 'test_resources', 'image.tif')
+        self.parseID = 'Name_Well'
+        self.separator = '_'
+        self.channelInfo = 'RFP_GFP'
+        self.channelToSegment = 'RFP'
         self.numFrames = 90
         self.frameInterval = 1
         self.frameToSegment = 'all'
-        self.Expnote = "test note"
+        self.Expnote = 'test note'
+        self.test_folder = os.path.join('tests', 'test_resources',
+                                        self.date + "_" + self.expID)
         yield
-        print("tearing down...")
-        shutil.rmtree('tmp')
-    
+        print('Breaking down...')
+        shutil.rmtree(self.test_folder)
+
+        
     def test_initialization(self, setup):
         # Test if the Experiment object is initialized correctly
         experiment = Experiment(self.date, self.expID, self.path, self.parseID,
@@ -43,7 +44,7 @@ class TestExperimentClass:
         assert experiment.frameInterval == self.frameInterval
         assert experiment.frameToSegment == 'all'
         assert experiment.Expnote == self.Expnote
-        assert hasattr(experiment, "folder")
+        assert hasattr(experiment, 'folder')
 
     def test_folder_creation(self, setup):
         # Test if the folder is created correctly
@@ -51,7 +52,8 @@ class TestExperimentClass:
                                 self.separator, self.channelInfo, self.channelToSegment,
                                 self.numFrames, self.frameInterval, self.frameToSegment,
                                 self.Expnote)
-        assert os.path.exists(experiment.folder)
+        new_folder_path = os.path.join(experiment.folder, self.date + '_' + self.expID)
+        assert os.path.exists(new_folder_path)
 
     def test_info_file_creation(self, setup):
         # Test if the info_exp.txt file is created correctly
@@ -59,6 +61,6 @@ class TestExperimentClass:
                                 self.separator, self.channelInfo, self.channelToSegment,
                                 self.numFrames, self.frameInterval, self.frameToSegment,
                                 self.Expnote)
-        info_file_path = os.path.join(experiment.folder, self.date + "_" + self.expID,
-                                      "info_exp.txt")
+        info_file_path = os.path.join(experiment.folder, self.date + '_' + self.expID,
+                                      'info_exp.txt')
         assert os.path.exists(info_file_path)

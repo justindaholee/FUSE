7aa4ed9b09d1f05caf10f3b005c43850f00b13a9
shanizu <77214955+shanizu@users.noreply.github.com>
2023-11-05 11:27:33 -0800

    adds score weight functionality

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+from fuse_toolkit import __version__
 
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
   
 setup(
     name='fuse_toolkit',
-    version='0.1.0b6',
+    version=__version__,
     description='FUSE toolkit supports fluorescent cell image alignment and analysis.',
     long_description=long_description,
     long_description_content_type='text/markdown',

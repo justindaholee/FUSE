5193703000c073306fb56792150c84af477052e3
shanizu <77214955+shanizu@users.noreply.github.com>
2024-02-27 10:51:36 -0800

    updates version

--- fuse_toolkit/__init__.py
+++ fuse_toolkit/__init__.py
@@ -6,7 +6,7 @@ fluorescent cell images, including functions for frame-by-frame analysis,
 image processing, and signal derivation.
 """
 
-__version__ = "0.1.0b17"
+__version__ = "0.1.0b18"
 __author__ = 'Shani Zuniga'
 __credits__ = 'Berndt Lab, University of Washington'
 
